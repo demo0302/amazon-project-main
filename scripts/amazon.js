ea8b19478ad5a488c4985552b3ddd59b51990b1a
demo2216 <nandinisingh1622@gmail.com>
2025-01-14 23:21:41 +0530

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,28 +1,4 @@
-const products=[{image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  price: 1090,
-  rating:{
-    star: 4.5,
-    count: 87
-  }
-},
-{image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  price: 2095,
-  rating:{
-    star: 4,
-    count: 127
-  }
-},
-{image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt 2 Pack',
-  price: 799,
-  rating: {
-    star: 4.5,
-    count: 56
-  }
 
-}];
 let producthtml=''
 products.forEach((product) => {
 producthtml= producthtml + `<div class="product-container">
