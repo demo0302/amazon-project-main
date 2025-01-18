977a74ff98946c6f6e0c9aa34dfdbbb58d44a5bf
demo2216 <nandinisingh1622@gmail.com>
2025-01-18 12:26:15 +0530

    add delete button interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem) => {
   });
 cartSummaryHTML+=
   `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: Tuesday, June 21
   </div>
@@ -91,7 +91,10 @@ cartSummaryHTML+=
 `
 }); 
 document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
-document.querySelector('.js-delete-link').forEach((link)=>{LocalDrink.addEventListener('click', ()=>{
+document.querySelectorAll('.js-delete-link').forEach((link)=>{link.addEventListener('click', ()=>{
 const productId=link.dataset.productId;
 removeFromCart(productId);
+const container=
+document.querySelector(`.js-cart-item-container-${productId}`);
+container.remove();
 })});
